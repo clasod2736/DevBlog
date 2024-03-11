5cf725b9680caf0fb55fc75ea87bd72647bf2b8d
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-03-11 23:46:31 +1100

    finished second post

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('postcss-nested'),
+    {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  ],
+};
